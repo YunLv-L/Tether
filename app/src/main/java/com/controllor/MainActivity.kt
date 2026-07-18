fc800ef85e825f7e7f033e65928422f28b93b8c9
YunLv <luoshaoye.yyds.0812@gmail.com>
2026-07-19 07:25:35 +0800

    Fix Dependence Error

--- app/src/main/java/com/controllor/MainActivity.kt
+++ app/src/main/java/com/controllor/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material.icons.filled.Check
